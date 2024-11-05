eea65e4ec0778af3d726dd35461ef13b9273e862
korip2139 <korip2139@yandex.ru>
2024-11-05 17:39:52 +0300

    Добавлена модель для работы с заявками

--- app/product_app/models.py
+++ app/product_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from app.users.models import UserClient
 # Create your models here.
+
+class Application(models.Model):
+	number_app = models.CharField(max_length=12)
+	user_app = models.OneToOneField(UserClient, on_delete=models.CASCADE)
