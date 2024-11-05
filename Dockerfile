a7f8ac76b952a736762bce2f26f8c14ee95807e5
korip2139 <korip2139@yandex.ru>
2024-11-05 17:10:36 +0300

    Добавлено приложение с пользователями

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,8 @@ COPY app /app
 WORKDIR /app
 EXPOSE 8000
 
+RUN apk add postgresql-client build-base postgresql-dev
+
 RUN pip install -r /temp/requirements.txt
 RUN adduser --disabled-password app-user
 
--- app/web_app/settings.py
+++ app/web_app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users',
+    'reports',
+    'product_app',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'web_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
